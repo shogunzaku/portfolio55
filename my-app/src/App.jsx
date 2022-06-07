102ec966715f8a9775d7e6584b69b792f6e90e90
Shogun <shogunzaku@gmail.com>
2022-06-06 22:16:28 -0700

    created about component and added about image to about css

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,7 +1,13 @@
+import About from"./components/about/About"
 import Intro from"./components/intro/Intro"
 
 const App = () => {
-  return <div><Intro/></div>;
+  return ( 
+  <div>
+    <Intro />
+    <About />
+  </div>
+  );
 };
 
 export default App;
---
+++ my-app/src/components/about/About.jsx
@@ -0,0 +1,27 @@
+import "./about.css"
+
+const About = () => {
+    return (
+        <div className="a">
+            <div className="a-left">
+                <div className="a-card bg"></div>
+                <div className="a-card">
+                    <img
+                        src="https://images.pexels.com/photos/4164418/pexels-photo-4164418.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                        alt=""
+                        className="a-img"
+                    />
+                </div>
+            </div>
+            <div className="a-right"></div>
+            
+
+
+
+
+
+        </div>
+    )
+}
+
+export default About
