866524d7d66eb6ff0ec89e9dba849cc341ecf099
Shogun <shogunzaku@gmail.com>
2022-06-06 19:43:31 -0700

    added discription section

--- my-app/src/components/intro/Intro.jsx
+++ my-app/src/components/intro/Intro.jsx
@@ -5,7 +5,7 @@ const Intro = () => {
         <div className="i">
             <div className="i-left">
                 <div className="i-left-wrapper">
-                    <h2 className="i-intro">Hello my name is</h2>
+                    <h2 className="i-intro">Hello, My name is</h2>
                     <h1 className="i-name">Antonio Camarena</h1>
                     <div className="i-title">
                         <div className="i-title-wrapper">
@@ -16,6 +16,11 @@ const Intro = () => {
                             <div className="i-title-item">Streamer</div>
                         </div>
                     </div>
+                   <div className="i-desc">
+                   Highly motivated Full Stack Web Developer with experience in web applications and production support. 
+                   Committed to problem solving through proactive resourcefulness and strategic creativity. Team oriented and dedicated to leading with service. 
+                   Graduated from the University of Arizona with a Certificate of Completion in Full Stack Web Development in May of 2022.
+</div> 
                 </div>
             </div>
             <div className="i-right">right</div>
