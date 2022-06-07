6dcfa8fba65ddd196de2d76cdbb01ad85a8fca02
Shogun <shogunzaku@gmail.com>
2022-06-06 18:50:01 -0700

    added font styling and animation to css

--- my-app/src/components/intro/Intro.jsx
+++ my-app/src/components/intro/Intro.jsx
@@ -1,9 +1,26 @@
+import "./intro.css"
+
 const Intro = () => {
-return (
-<div>
-This is intro
-</div>
-)
+    return (
+        <div className="i">
+            <div className="i-left">
+                <div className="i-left-wrapper">
+                    <h2 className="i-intro">Hello my name is</h2>
+                    <h1 className="i-name">Antonio Camarena</h1>
+                    <div className="i-title">
+                        <div className="i-title-wrapper">
+                            <div className="i-title-item">Full Stack Web Developer</div>
+                            <div className="i-title-item">Content Creator</div>
+                            <div className="i-title-item">Artist</div>
+                            <div className="i-title-item">Musician</div>
+                            <div className="i-title-item">Streamer</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="i-right">right</div>
+        </div>
+    )
 }
 
 export default Intro
